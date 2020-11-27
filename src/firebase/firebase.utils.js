f5b1a46a6cd978e1c9f8cee1db9ca48505b108ba
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-27 17:20:15 +0100

    Add register and useGoogleProvider functions

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-
+// Development Configure
 const devConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -12,16 +12,31 @@ const devConfig = {
     appId: process.env.REACT_APP_APP_ID
 };
 
+// Production Configure
 const prodConfig = {};
 
 const config = process.env.NODE_ENV === 'development' ? devConfig : prodConfig;
 
 class Firebase {
     constructor() {
-
         firebase.initializeApp(config);
         this.firebaseAuth = firebase.auth();
+        console.log(firebase);
+        console.log(this.firebaseAuth);
     }
+
+    // register button
+    register(email, password) {
+        this.firebaseAuth.createUserWithEmailAndPassword(email, password);
+    }
+
+    // Sign Up with Google
+    useGoogleProvider() {
+        const googleProvider = new firebase.auth.GoogleAuthProvider();
+        googleProvider.setCustomParameters({ prompt: "select_account" });
+        this.firebaseAuth.signInWithPopup(googleProvider);
+    }
+
 }
 
 export default new Firebase();
