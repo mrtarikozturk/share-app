a6b109e6f260bc19e33700b8e7c9be288ee64e94
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-30 23:48:36 +0100

    Created Main page

---
+++ src/pages/Main.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import { Container, Grid, capitalize } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+import Mediacard from '../components/MediaCard';
+
+// style object
+const styleFunc = makeStyles((theme) => ({
+    wrapper: {
+        // TODO: stillendirmeyi yap
+    }
+}))
+
+const Main = () => {
+
+    // constants
+    const { REACT_APP_API_BASE_URL, REACT_APP_API_TOKEN } = process.env;
+    const mainStyles = styleFunc();
+
+    // states
+    const [userList, setUserList] = useState();
+
+    // useEffects
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        const response = await axios.get(`${REACT_APP_API_BASE_URL}/user`, {
+            headers: {
+                'app-id': REACT_APP_API_TOKEN,
+            }
+        });
+        setUserList(response?.data?.data);
+        console.log(response)
+    }
+    return (
+        <Container className={mainStyles.wrapper} maxWidth={'lg'}>
+            <Grid container spacing={3}>
+                {
+                    userList?.map((item, index) => (
+
+                        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                            <Mediacard
+                                data={item}
+                            />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        </Container >
+    )
+}
+
+export default Main
