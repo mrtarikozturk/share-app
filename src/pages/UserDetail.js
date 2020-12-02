cf57a7b704ac8f86f37057da9547d4d127664b44
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-12-02 23:44:11 +0100

    Add style and circularProgress component

--- src/pages/UserDetail.js
+++ src/pages/UserDetail.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Grid, capitalize } from '@material-ui/core';
+import { CircularProgress, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useParams } from 'react-router-dom';
 import { fetchData } from '../helper/FetchData';
-import Main from './Main';
+import { Typography } from '@material-ui/core';
+import { dateFormater } from '../helper/FormatDate';
+
 
 // style object
 const styleFunc = makeStyles((theme) => ({
     wrapper: {
         // TODO: stillendirmeyi yap
+        marginTop: "10rem",
+        height: "calc(100vh - 19.0625rem)",
+        textAlign: "center",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
     }
 }))
 
@@ -26,12 +35,24 @@ const UserDetail = () => {
         fetchData(`/user/${id}`)
             .then(response => setUserDetail(response))
             .catch(error => console.error(error))
-    }, [])
+    }, [id])
+
 
     return (
         <Container className={detailStyles.wrapper}>
-            {id}
-            {JSON.stringify(userDetail)}
+            {
+                !userDetail
+                    ? (<CircularProgress />)
+                    : <React.Fragment>
+                        <img src={userDetail?.picture} alt='user' />
+                        <Typography variant='h4' >{userDetail?.firstName}</Typography>
+                        <Typography variant='h4' >{userDetail?.lastName}</Typography>
+                        <Typography variant='h4' >
+                            {dateFormater(userDetail?.registerDate)}
+                        </Typography>
+                        <Typography variant='h4' >{userDetail?.phone}</Typography>
+                    </React.Fragment>
+            }
         </Container>
     )
 }
@@ -41,3 +62,5 @@ export default UserDetail
 
 // TODO: Burayi api sayfasindaki gibi detaylandir ve haritayi da kullan.
 // TODO: https://stackoverflow.com/questions/39857425/react-page-keep-footer-at-the-bottom-of-the-page
+// TODO: move datetime to helper 
+// TODO: carlardaki butonlari guzelletir.
