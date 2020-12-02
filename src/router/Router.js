9989440a18819a797181694eac2517297662ab59
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-12-02 23:48:03 +0100

    Add Forgot Password Route and Footer component

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,20 +1,29 @@
+import { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from '../pages/Main';
 import Signup from '../pages/Signup';
 import Signin from '../pages/Signin';
 import Navbar from '../components/Navbar';
 import UserDetail from '../pages/UserDetail';
+import Footer from '../components/Footer';
+import { FirebaseAuthContext } from '../context/AuthContext';
+import ForgotPassword from '../pages/ForgotPassword.js';
 
 function AppRouter() {
+
+    const { currentUser } = useContext(FirebaseAuthContext);
+
     return (
         <Router>
             <Navbar />
             <Switch>
                 <Route exact path='/register' component={Signup} />
                 <Route exact path='/login' component={Signin} />
-                <Route exact path='/user/:id' component={UserDetail} />
+                <Route exact path='/forgot-password' component={ForgotPassword} />
+                <Route exact path='/user/:id' component={currentUser ? UserDetail : Signin} />
                 <Route path='/' component={Main} />
             </Switch>
+            <Footer />
         </Router>
     );
 }
